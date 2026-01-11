6db001081e8a2574344243429ccf3475c0bde789
Chao <114423476+ChaoIsBalling@users.noreply.github.com>
2026-01-11 02:41:55 +0100

    R17

---
+++ MARPR17/MARPR17/MARPR17.cpp
@@ -0,0 +1,80 @@
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (MARPXX) de los autores de la solución.
+ *
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+using namespace std;
+ // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+bool resuelveCaso() {
+
+    // leer los datos de la entrada
+    int N;
+    cin >> N;
+    if (!std::cin)  // fin de la entrada
+        return false;
+
+    priority_queue<int>libros;
+    int n;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> n;
+        libros.push(n);
+    }
+    int count = 0;
+    int descuento = 0;
+    while (!libros.empty())
+    {
+        count++;
+        if (count == 3)
+        {
+            descuento += libros.top();
+            count = 0;
+        }
+        libros.pop();
+    }
+    cout <<descuento << endl;
+    // resolver el caso posiblemente llamando a otras funciones
+
+    // escribir la solución
+
+    return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    std::ifstream in("casos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+    // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+#endif
+    return 0;
+}
